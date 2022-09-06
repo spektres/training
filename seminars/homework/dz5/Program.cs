10b4ba73219cc41b928b15cc0c1230012ff102bf
spektres <spektres@mail.ru>
2022-09-06 11:43:13 +0300

    dz5

--- seminars/homework/dz5/Program.cs
+++ seminars/homework/dz5/Program.cs
@@ -1,10 +1,12 @@
-﻿Console.WriteLine("Введите номер задания, которое хотите проверить: 34, 36 или 38.");
+﻿Console.Clear();
+Console.WriteLine("Введите номер задания, которое хотите проверить: 34, 36 или 38.");
 int task = Convert.ToInt32(Console.ReadLine());
 
 int taskOne()
 {
-    Console.WriteLine("ЗАДАЧА 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
-    int[] array = new int[10];
+    Console.WriteLine("Введите колличество элементов массива (элементы будут с рандомными значениями)");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
     int count = 0;
     for(int i = 0; i < array.GetLength(0); i++)
     {
@@ -16,8 +18,9 @@ int taskOne()
 
 int taskTwo()
 {
-    Console.WriteLine("ЗАДАЧА 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
-    int[] array = new int[10];
+    Console.WriteLine("Введите колличество элементов массива (элементы будут с рандомными значениями)");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
     int sum = 0;
     for(int i = 0; i < array.GetLength(0); i++)
     {
@@ -27,9 +30,33 @@ int taskTwo()
     return sum;
 }
 
+double taskThree()
+{
+    Console.WriteLine("Укажите колличество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    double [] array = new double[size];
+    double min = 100; double max = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е число (от 0 до 100)");
+        array[i] = Convert.ToDouble(Console.ReadLine());
+        if(array[i] < min) min = array[i];
+        if(array[i] > max) max = array[i];
+    }
+    double result = max - min;
+    return result;
+}
+
 switch(task)
 {
-    case 34: Console.WriteLine($"Колличество четных чисел в массиве равно {taskOne()} из 10."); break;
-    case 36: Console.WriteLine($"Сумма нечетных элементов массива равна {taskTwo()}"); break;
+    case 34: 
+    Console.WriteLine("ЗАДАЧА 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+    Console.WriteLine($"Колличество четных чисел в массиве равно {taskOne()}."); break;
+    case 36: 
+    Console.WriteLine("ЗАДАЧА 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+    Console.WriteLine($"Сумма нечетных элементов массива равна {taskTwo()}"); break;
+    case 38: 
+    Console.WriteLine("ЗАДАЧА 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {taskThree()}"); break;
 }
 
